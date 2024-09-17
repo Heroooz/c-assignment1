de9a21ca7382fc6b4a7cdea99ecbef49fc21f90f
Heroooz <110030617+Heroooz@users.noreply.github.com>
2024-09-17 14:12:58 +0200

    moved code

--- memorymanager.c
+++ memorymanager.c
@@ -1,2 +1,31 @@
-#include "memorymanager.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
+void mem_init(size_t size)
+{
+
+}
+
+void* mem_alloc(size_t size)
+{
+
+}
+
+void mem_free(void* block)
+{
+
+}
+
+void* mem_resize(void* block, size_t size)
+{
+
+}
+
+void mem_deinit()
+{
+
+}
+
+#endif
