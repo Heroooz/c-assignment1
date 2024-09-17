3dc3e38abe0d640414450d4619ac5e86918a71b0
Heroooz <110030617+Heroooz@users.noreply.github.com>
2024-09-17 14:07:17 +0200

    add header file

---
+++ memorymanager.h
@@ -0,0 +1,20 @@
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdatomic.h>
+
+
+void mem_init(size_t size);
+
+void* mem_alloc(size_t size);
+
+void mem_free(void* block);
+
+void* mem_resize(void* block, size_t size);
+
+void mem_deinit();
+
+#endif
