7f366e8a0b38d62957f55472dce95e7555b428ec
Heroooz <110030617+Heroooz@users.noreply.github.com>
2024-09-30 17:14:07 +0200

    new func to memorymanager

--- memory_manager.c
+++ memory_manager.c
@@ -10,25 +10,37 @@ unsigned char* end;
 static void* memory_pool = NULL;
 size_t size_of_pool;
 
+void set_bit(unsigned char* arr, size_t index) {
+    arr[index / 8] |= (1 << (index % 8));
+}
+
+void clear_bit(unsigned char* arr, size_t index) {
+    arr[index / 8] &= ~(1 << (index % 8));
+}
+
+bool get_bit(unsigned char* arr, size_t index) {
+    return (arr[index / 8] & (1 << (index % 8)));
+}
+
 // Initialization function: creates a memory pool of the given size
 void mem_init(size_t size) {
     memory_pool = malloc(size);  // Allocate memory pool
-    start = calloc((size + 7) / 8, 1);
-    end = calloc((size + 7) / 8, 1);
+    start = calloc((size + 7) / 8, 1);  // Allocate bit array for start
+    end = calloc((size + 7) / 8, 1);    // Allocate bit array for end
     size_of_pool = size;
 }
 
 // Allocation function: finds the first free block that fits the requested size
 void* mem_alloc(size_t size) {
-    if (size == 0) {
-        return NULL;  // Cannot allocate zero bytes
+    if (size == 0 || size > size_of_pool) {
+        return NULL;  // Cannot allocate zero bytes or more than the pool size
     }
 
     // Find the first free block that fits the requested size
-    for (size_t i = 0; i < size_of_pool; i++) {
+    for (size_t i = 0; i <= size_of_pool - size; i++) {
         bool block_free = true;
         for (size_t j = 0; j < size; j++) {
-            if (start[i + j] != 0) {
+            if (get_bit(start, i + j)) {
                 block_free = false;
                 break;
             }
@@ -37,7 +49,7 @@ void* mem_alloc(size_t size) {
         if (block_free) {
             // Mark the block as used
             for (size_t j = 0; j < size; j++) {
-                start[i + j] = 1;
+                set_bit(start, i + j);
             }
             return (void*)((unsigned char*)memory_pool + i);
         }
@@ -45,6 +57,7 @@ void* mem_alloc(size_t size) {
 
     return NULL;  // No free block found
 }
+
 
 // Deallocation function: marks a block as free
 void mem_free(void* block) {
@@ -54,8 +67,8 @@ void mem_free(void* block) {
     }
 
     // Mark the block as free
-    for (size_t i = offset; i < size_of_pool && start[i] == 1; i++) {
-        start[i] = 0;
+    for (size_t i = offset; i < size_of_pool && get_bit(start, i); i++) {
+        clear_bit(start, i);
     }
 }
 
@@ -80,7 +93,6 @@ void* mem_resize(void* block, size_t size) {
     for (size_t i = offset; i < size_of_pool && start[i] == 1; i++) {
         current_size++;
     }
-
     if (current_size >= size) {
         return block;  // No need to resize
     }
@@ -105,4 +117,6 @@ void mem_deinit() {
     free(end);
     free(start);
     free(memory_pool);
+    memory_pool = NULL;
+    size_of_pool = 0;
 }
